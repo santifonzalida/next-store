80cd29d35d4720a382ff5eb7a455a3ade4a7140d
santifonzalida <santiifonzalida@gmail.com>
2024-06-10 19:20:45 -0300

    loading and fech products

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const { hostname } = require('os')
 const path = require('path')
 
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'src/sass')],
-        prependData: `@import "main.sass"`,
-      },
+  sassOptions: {
+      includePaths: [path.join(__dirname, 'src/sass')],
+      prependData: `@import "main.sass"`,
+    },
+  images: {
+    remotePatterns: 
+    [
+      {
+        hostname: 'cdn.shopify.com',
+        protocol: 'https'
+      }
+    ]
+  }
 }
