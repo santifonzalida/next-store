b2877f7b2a72915fca48a3fc374a0de6afff20bf
santifonzalida <santiifonzalida@gmail.com>
2024-06-18 19:07:18 -0300

    scale folding

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,20 +1,6 @@
 import styles from './MainProducts.module.sass'
 import Image from 'next/image';
-
-const getProducts = async () => {
-    try {
-        const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
-            headers: {
-                "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
-            }
-        })
-        const { products } = await response.json();
-        return products; 
-    } catch (error) {
-        console.log(error);
-    }   
-}
-
+import { getProducts } from 'app/services/shopify';
 
 export const MainProducts = async () => {
 
